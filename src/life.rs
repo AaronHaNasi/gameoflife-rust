3aa3cb60d111ad9d67f1660531bae10dff400edc
Aaron <19242692+AaronHaNasi@users.noreply.github.com>
2021-04-09 19:16:47 -0400

    Update life.rs
    More small fixes

--- src/life.rs
+++ src/life.rs
@@ -6,11 +6,14 @@ fn toString(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 }
 
 fn loadGridFromFile(filename: String, rows: *mut u8, cols: *mut u8) {
+
     let mut file = File::open(filename);
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     contents.split_whitespace();
+
     let mut read_contents = contents.lines();
+
     rows = read_contents.next();
     cols = read_contents.next();
 
@@ -18,7 +21,10 @@ fn loadGridFromFile(filename: String, rows: *mut u8, cols: *mut u8) {
 
     for i in 0..rows {
         grid.push(Vec::new()):
-    }
+        for j in 0..cols {
+            grid[i].push(read_contents.next());
+        }
+    };
 }
 
 fn saveGridToFile(filename: str, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
