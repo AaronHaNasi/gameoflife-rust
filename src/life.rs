4412d3614e2b14111c66c0f640f9de2327f008a1
Aaron <19242692+AaronHaNasi@users.noreply.github.com>
2021-04-09 19:11:40 -0400

    Update life.rs
    updated str to String

--- src/life.rs
+++ src/life.rs
@@ -1,29 +1,29 @@
-use std::fs::File; 
+use std::fs::File;
 use std::io::Prelude::*;
 
 fn toString(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
 }
 
-fn loadGridFromFile(filename: str, rows: *u8, cols: *u8) {
-    let mut file = File::open(filename); 
-    let mut contents = String::new(); 
-    file.read_to_string(&mut contents)?; 
-    contents.split_whitespace(); 
-    let mut read_contents = contents.lines(); 
-    rows = read_contents.next(); 
-    cols = read_contents.next(); 
+fn loadGridFromFile(filename: String, rows: *u8, cols: *u8) {
+    let mut file = File::open(filename);
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    contents.split_whitespace();
+    let mut read_contents = contents.lines();
+    rows = read_contents.next();
+    cols = read_contents.next();
 
-    let mut grid = Vec::new(); 
+    let mut grid = Vec::new();
     
     for i in 0...rows {
-        grid.push(Vec::new()): 
+        grid.push(Vec::new()):
     }
 }
 
 fn saveGridToFile(filename: str, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
-} 
+}
 
 fn copyGrid(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
