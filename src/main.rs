75d437f58b8796640196ed6904022f4f2c94a741
Aaron <kingjojo3@gmail.com>
2021-04-09 17:11:26 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::env;
+use std::process; 
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() != 2 {
+        print!("Wrong number of arguments to program. This program requires a filename as an argument.\nUsage: ./driver filename\nExample: ./driver beacon.gol\n");
+        process::exit(1); 
+    }
+    
+    let mut rows: u8; 
+    let mut cols: u8; 
+    let grid: *mut u8;   
+}
