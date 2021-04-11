b7cfe54241c280294f581055bc36e7ec0a3ce779
Aaron <kingjojo3@gmail.com>
2021-04-11 02:03:24 -0400

    Got grid to display properly

--- src/life.rs
+++ src/life.rs
@@ -2,45 +2,55 @@ use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
 
-fn toString(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
-
+pub fn to_string(grid: Vec<Vec<u8>>) -> String {
+    let mut return_string: String = "".to_string(); 
+    
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            if grid[i][j] == 0 {
+                return_string.push_str(" . ");
+            } else {
+                return_string.push_str(" X "); 
+            }
+        }
+        return_string.push('\n'); 
+   }
+    return_string
 }
 
-pub fn loadGridFromFile(filename: String) {
+pub fn load_grid_from_file(filename: String) -> (Vec<Vec<u8>>, u8, u8) {
 
-    // let mut file = File::open(filename);
-    let mut contents = fs::read_to_string(filename);
-    println!("{:?}", contents); 
-    // file.read_to_string(&mut contents);
-    // contents.split_whitespace();
+    let mut contents = fs::read_to_string(filename).unwrap(); 
+    let mut iterator = contents.split_whitespace(); 
 
     // let mut read_contents = contents.lines();
 
-    // let rows = read_contents.next();
-    // let cols = read_contents.next();
+    let rows: u8 = iterator.next().unwrap().parse().unwrap();
+    let cols: u8 = iterator.next().unwrap().parse().unwrap();
 
-    // let mut grid = Vec::new();
+    let mut grid = vec![vec![0; rows.into()]; cols.into()];
+    
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            grid[i][j] = iterator.next().unwrap().parse().unwrap(); 
+        }
+    }
 
-    /*for i in 0..rows {
-        grid.push(Vec::new()):
-        for j in 0..cols {
-            grid[i].push(read_contents.next());
-        };
-    };*/
+    (grid, rows, cols) 
 }
 
-pub fn saveGridToFile(filename: String, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
-
-}
-
-fn copyGrid(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
+pub fn save_grid_to_file(filename: String, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
 }
 
-pub fn mutateGrid(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
+fn copy_grid(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
 }
 
-fn nbrOfNeighbors(i: u8, j: u8, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
+pub fn mutate_grid(rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
+
+}
+
+fn nbr_of_neighbors(i: u8, j: u8, rows: u8, cols: u8, grid: Vec<Vec<u8>>) {
 
 }
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,18 @@ fn main() {
         print!("Wrong number of arguments to program. This program requires a filename as an argument.\nUsage: ./driver filename\nExample: ./driver beacon.gol\n");
         process::exit(1);
     }
-
+    
     let mut rows: u8;
     let mut cols: u8;
-    let mut gameInfo = life::loadGridFromFile(args[1]);
+    
+    let filename = &args[1];
+    
+    let mut game_info = life::load_grid_from_file(filename.to_string());
+    
+    let grid = game_info.0;
+    let rows = game_info.1;
+    let cols = game_info.2;
+
+    let display_string = life::to_string(grid); 
+    println!("{}\n", display_string);  
 }
